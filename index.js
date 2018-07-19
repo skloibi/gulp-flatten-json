937cb7939a95daad18ad3fd6e34d529d2be7ebfd
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-07-19 23:12:36 +0100

    Remove deprecated dependencies (gulp-util, minimatch)

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
  * @since 1.0 (03/27/2017)
  */
 const through     = require('through2');
-const gutil       = require('gulp-util');
-const PluginError = gutil.PluginError;
+const log         = require('fancy-log');
+const c           = require('ansi-colors');
+const PluginError = require('plugin-error');
 
-const flatten = require('./flatten');
+const flatten     = require('./flatten');
 const PLUGIN_NAME = 'gulp-flatten-json';
 
 module.exports = function(options) {
@@ -20,15 +21,15 @@ module.exports = function(options) {
         if (file.isStream()) {
             // file.contents is a Stream - https://nodejs.org/api/stream.html
             this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
-        }
-        else{
+        } else {
             let obj = null;
-            try{
+            try {
                 obj = JSON.parse(file.contents);
-            }
-            catch(e){
-                gutil.log('gulp-inline-base64:',
-                    gutil.colors.red('not a json file') + gutil.colors.gray(' (' + file.path + ')'));
+            } catch(e) {
+                log(
+                    'gulp-inline-base64:', 
+                    c.red('not a json file') + c.gray(' (' + file.path + ')')
+                );
                 return callback(null, file);
             }
             file.contents = Buffer.from(JSON.stringify(flatten(obj)), 'utf8' );
